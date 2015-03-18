7005a4b2dd0166c9d2503cb7a6b381933f90e786
Maria Bikova <bikova.marija@gmail.com>
2015-03-18 12:07:58 +0000

    Bootstrap template, form, options

--- app.rb
+++ app.rb
@@ -2,10 +2,23 @@ require 'rubygems'
 require 'sinatra'
 
 
+#get '/' do
+	#erb :index
+#end
+
 get '/' do
-	erb :index
+	erb :form
 end
 
+post '/' do
+	@Name = params[:InputName] 
+	@Email =params[:InputEmail]
+	@Age = params[:InputAge]
+	@Vegetarian = params[:InputVeg]
+	
+	
+	redirect '/thanks'
+end
 
 get '/thanks' do
 	erb :thanks
